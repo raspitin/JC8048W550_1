816170b701d2acda56464eb4725f2a6c8b0d03d1
raspitin <30929800+raspitin@users.noreply.github.com>
2025-11-29 16:42:12 +0100

    sfarfallio

--- include/ui.h
+++ include/ui.h
@@ -1,17 +1,18 @@
 #ifndef UI_H
 #define UI_H
+
 #include <Arduino.h>
 
 // Inizializza tutte le schermate (Home, Caldaia, Setup, Impegni)
 void ui_init_all();
 
-// Aggiorna i dati dinamici (Orario, WiFi, ecc.)
+// Aggiorna i dati dinamici (Orario, WiFi, Pulsante Manuale)
 void update_ui();
 
-// Aggiorna meteo corrente
+// Aggiorna meteo corrente (con icona)
 void update_current_weather(String temp, String desc, String iconCode);
 
-// Aggiorna un box previsione
+// Aggiorna un box previsione (giorni successivi)
 void update_forecast_item(int index, String day, String temp, String iconCode);
 
 #endif
